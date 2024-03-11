f7f85ed37ed0925040a686504715568495d60503
tom <tomraz@gmail.com>
2024-03-11 10:16:53 +0200

    trainer

---
+++ trainer.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
+
+# Train the model
+results = model.train(data='/home/tom/Projects/bv-play-break-detection/service/research/scripts/actions.yaml', epochs=60, imgsz=900)
